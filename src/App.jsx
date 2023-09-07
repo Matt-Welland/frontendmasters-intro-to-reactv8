3ff1004b7587f27e2c6d945eb4b688d6f1e5abe3
Matt Welland <mattwelland22@gmail.com>
2023-09-07 12:01:30 +0100

    Upgrading to JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,16 @@
-import React from "react";
 import { createRoot } from "react-dom";
-
-const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed),
-    ]);
-};
+import Pet from "./Pet";
 
 const App = () => {
-    return React.createElement("div", { id: "my-id" }, [
-        React.createElement("h1", {}, "Adopt Me!"),
-        React.createElement(Pet, {
-            animal: "Dog",
-            name: "Luna",
-            breed: "Havanese",
-        }),
-        React.createElement(Pet, {
-            animal: "Bird",
-            name: "Pepper",
-            breed: "Cockatiel",
-        }),
-        React.createElement(Pet, {
-            animal: "Cat",
-            name: "Doink",
-            breed: "Mixed",
-        }),
-    ]);
+    <div>
+        <h1>Adopt me!</h1>
+        <Pet name={"Luna"} animal={"Dog"} breed={"Havanese"} />
+        <Pet name={"Pepper"} animal={"Bird"} breed={"Cockatiel"} />
+        <Pet name={"Doink"} animal={"Cat"} breed={"Mixed"} />
+    </div>;
 };
 
 // Render to the root which is outputted to the DOM by ReactDOM
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
